9439108a671cd49f31cc934e1f75ce826c420c24
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-07 23:06:04 +0530

    adding

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 component=$1
 environment=$2
+appVersion=$3
 dnf install ansible -y
 pip3.9 install boto3 botocore 
 ansible-pull -i localhost, -U https://github.com/Shashikanth-Kammari/expense-ansible-rules-tf.git main.yaml -e 
-component=$component -e env=$environment
+component=$component -e env=$environment -e app_version=$appVersion
